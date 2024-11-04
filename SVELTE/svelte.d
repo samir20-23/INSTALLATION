3ae74d162dc5b3c1c538ee7754fd2a26de709263
samir20-23 <tistenow@gmail.com>
2024-11-04 10:18:21 +0100

    svelte

---
+++ SVELTE/svelte.d
@@ -0,0 +1,6 @@
+0: T -> node -v and npm -v (to check your Node.js and npm versions).
+1: T -> npx degit sveltejs/template my-svelte-app (to create a new Svelte project).
+2: T -> cd my-svelte-app (to navigate into the project folder).
+3: T -> npm install (to install dependencies).
+4: T -> npm run dev (to start the development server).
+5: T -> Go to http://localhost:5000/ (to view your Svelte app).
